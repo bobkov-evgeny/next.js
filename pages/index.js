0f291f4c14c8abb1f44ab334b76b225d17abb0e6
Evgenii <freeedayy@gmail.com>
2022-05-15 21:38:39 +0300

    fetching data

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,35 @@
 import Heading from "../components/Heading";
 import styles from "../styles/Home.module.scss"
 
-const Home = () => {
-  return (
-      <div className={styles.wrapper}>
-        <Heading text="Hello world!"/>
-      </div>
-  )
+export const getStaticProps = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await response.json();
+
+    if (!data) {
+        return {
+            notFound: true
+        }
+    }
+
+    return {
+        props: {contacts: data}
+    }
+};
+
+const Home = ({contacts}) => {
+    return (
+        <div className={styles.wrapper}>
+
+            <Heading text="Hello world!"/>
+            <ul>
+                {contacts && contacts.map(({id, name, email}) => (
+                    <li key={id}>
+                        <strong>{name}</strong>{email}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 
 export default Home;
