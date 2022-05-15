affda8fd5593d0392ce2ce92fd6a1482f420d6ea
Evgenii <freeedayy@gmail.com>
2022-05-15 21:15:54 +0300

    Page navigation & redirect

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,10 @@
 import Heading from "./Heading";
+import Navbar from "./Navbar";
 
 const Header = () => {
     return (
         <header>
-            <Heading tag="h3" text="Header" />
+            <Navbar />
         </header>
     )
 }
--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,16 @@
 import Heading from "../components/Heading";
+import {useEffect} from "react";
+import {useRouter} from "next/router";
 import styles from "../styles/404.module.scss";
 
 const Error = () => {
+    const router = useRouter();
+
+    useEffect(() => {
+        setTimeout(() => {
+            router.push('/');
+        }, 3000)
+    }, [router])
     return (
         <div className={styles.wrapper}>
             <Heading text="Error 404"/>
